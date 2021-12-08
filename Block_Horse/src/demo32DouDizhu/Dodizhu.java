66235f3be31f803898047670686a7b586e201532
Stephen Jun <1307612178@qq.com>
2021-12-08 16:16:29 +0800

    Java基础学习第12-1次

--- Block_Horse/src/demo32DouDizhu/Dodizhu.java
+++ Block_Horse/src/demo32DouDizhu/Dodizhu.java
@@ -18,14 +18,21 @@ import java.util.List;
 public class Dodizhu {
     public static void main(String[] args) {
 //        1.准备牌
+
+//        创建一个HashMap集合用来储存扑克牌
         HashMap<Integer, String> poker = new HashMap<>();
+//        创建一个ArrayList集合用来储存扑克牌序号
         List<Integer> indexPoker = new ArrayList<>();
+//        创建两个ArrayList集合用来储存花色和数字
         List<String> flowew = List.of("红桃", "黑桃", "梅花", "方块");
         List<String> number = List.of("2", "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3");
+//        将大王和小王注入poker集合
         poker.put(1, "大王");
         poker.put(2, "小王");
+//        将已使用的索引123注入indexPoker集合
         Collections.addAll(indexPoker, 1, 2, 3);
         int index = 3;
+//        嵌套循环花色和数字 组装好扑克
         for (String n : number) {
             for (String f : flowew) {
                 poker.put(index, f + n);
@@ -33,17 +40,20 @@ public class Dodizhu {
                 index++;
             }
         }
-//       2. 洗牌
+//       2. 洗牌将牌序打乱
         Collections.shuffle(indexPoker);
-        System.out.println(indexPoker);
+//        打乱牌序后的扑克牌
+//        System.out.println(indexPoker);
 //        3.发牌
+
         ArrayList<Integer> Bojac = new ArrayList<>();
         ArrayList<Integer> Steph = new ArrayList<>();
         ArrayList<Integer> Crtrl = new ArrayList<>();
         ArrayList<Integer> DiPai = new ArrayList<>();
-        for (int i = 0; i < indexPoker.size(); i++) {
+//        因为index序号是从1开始因此循环遍历indexpoker时要从1 开始循环
+        for (int i = 1; i < indexPoker.size(); i++) {
             Integer s = indexPoker.get(i);
-            if (i < 3) {
+            if (i >=52) {
                 DiPai.add(s);
             } else if (i % 3 == 0) {
                 Bojac.add(s);
@@ -65,12 +75,18 @@ public class Dodizhu {
         System.out.println("底牌:");
         xun(DiPai, poker);
     }
+/*
+遍历方法
+ */
     public static void xun(ArrayList<Integer> list, HashMap<Integer, String> poker) {
 //        排序
         Collections.sort(list);
+//        遍历每个玩家的牌
         for (int i = 0; i < list.size(); i++) {
+//            使用HashMap的get方法用Key获取Value
             String s = poker.get(list.get(i));
-            System.out.print(s + " ");
+//            输出Value
+            System.out.print(s + " "+i);
         }
     }
 
